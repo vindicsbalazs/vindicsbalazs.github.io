8e5274faa590e4ea265ba308f244eac1472d70e8
vindicsbalazs <99795316+vindicsbalazs@users.noreply.github.com>
2022-09-23 00:45:04 +0200

    repair

---
+++ js/photo_group.js
@@ -0,0 +1,306 @@
+groups = [
+  {
+    "groupName": "Öröm szülők",
+    "groupList":[
+      {"nick" : "Sanyiapa", "name" : "Tóth Sándor"},
+      {"nick" : "Gyopszianya", "name" : "Soós Gyopár"},
+      {"nick" : "Peti (Apa)", "name" : "Vindics Péter"},
+      {"nick" : "Judit (Anya)", "name" : "Vindics Péterné"}
+    ]
+  },
+  {
+    "groupName": "Tóth család",
+    "groupList":[
+      {"nick" : "Sanyiapa", "name" : "Tóth Sándor"},
+      {"nick" : "Gyopszianya", "name" : "Soós Gyopár"},
+      {"nick" : "Lilla", "name" : "Kalocsa Lilla"},
+      {"nick" : "Boldi", "name" : "Tóth Boldizsár"},
+      {"nick" : "Lencsi", "name" : "Tóth Lenke"},
+      {"nick" : "Dani", "name" : "Dr. Abai Dániel"},
+      {"nick" : "Gellért", "name" : "Abai Gellért Hunor"},
+      {"nick" : "Virág", "name" : "Abainé Tóth Virág"},
+      {"nick" : "Máté", "name" : "Abainé Máté Csaba"}
+    ]
+  },
+  {
+    "groupName": "Tóth testvérek",
+    "groupList":[
+      {"nick" : "Boldi", "name" : "Tóth Boldizsár"},
+      {"nick" : "Lencsi", "name" : "Tóth Lenke"},
+      {"nick" : "Virág", "name" : "Abainé Tóth Virág"}
+    ]
+  },
+  {
+    "groupName": "Abai Család",
+    "groupList":[
+      {"nick" : "Dani", "name" : "Dr. Abai Dániel"},
+      {"nick" : "Gellért", "name" : "Abai Gellért Hunor"},
+      {"nick" : "Virág", "name" : "Abainé Tóth Virág"},
+      {"nick" : "Máté", "name" : "Abainé Máté Csaba"},
+      {"nick" : "Ambrus!", "name" : "Tamás Ambrus"},
+      {"nick" : "Orsi!", "name" : "Abai Orsolya"}
+    ]
+  },
+  {
+    "groupName": "Vindics Család",
+    "groupList":[
+      {"nick" : "Peti (Apa)", "name" : "Vindics Péter"},
+      {"nick" : "Juditka (Anya)", "name" : "Vindics Péterné"},
+      {"nick" : "Bence", "name" : "Rab Benedek"},
+      {"nick" : "Dorka", "name" : "Rab Dóra"},
+      {"nick" : "Tomi", "name" : "Vindics Tamás"},
+      {"nick" : "Eszti", "name" : "Erős Eszter"}
+    ]
+  },
+  {
+    "groupName": "Soós Enikő és családja",
+    "groupList":[
+      {"nick" : "Panni", "name" : "Szente Panna"},
+      {"nick" : "Imola", "name" : "Szente Imola"},
+      {"nick" : "Enikő", "name" : "Soós Enikő"},
+      {"nick" : "Lajos", "name" : "Sárközi Lajos"},
+      {"nick" : "Bori", "name" : "Fehér Sz Borbála"},
+      {"nick" : "Samu", "name" : "Fehér Sámuel Ferenc"},
+      {"nick" : "István", "name" : "Fehér István"}
+    ]
+  },
+  {
+    "groupName": "Nagyszülők",
+    "groupList":[
+      {"nick" : "Ei Mama", "name" : "Balás Endréné"},
+      {"nick" : "Papa", "name" : "Balás Endre"},
+      {"nick" : "Nagyi", "name" : "Dr. Soós Ferencné"},
+      {"nick" : "Sásdi Mama", "name" : "Tóth Tihamérné"},
+    ]
+  },
+  {
+    "groupName": "Kereszt szülők",
+    "groupList":[
+      {"nick" : "István", "name" : "Dr. Nagy István"},
+      {"nick" : "Rozi", "name" : "Zilizi Rozália"},
+      {"nick" : "Tamás!", "name" : "Székedi Tamás"},
+    ]
+  },
+  {
+    "groupName": "Óbíró család",
+    "groupList":[
+      {"nick" : "Norbi", "name" : "Nagy Norbert"},
+      {"nick" : "Kitti", "name" : "Óbíró Kitti"},
+      {"nick" : "István", "name" : "Joós István"},
+      {"nick" : "Réka", "name" : "Óbíró Réka"},
+      {"nick" : "Robi", "name" : "Óbíró Róbert"},
+      {"nick" : "Zsóka", "name" : "Óbíróné Balás Zsuzsanna"},
+      {"nick" : "Gergő", "name" : "Óbíró Gergő"}
+    ]
+  },
+  {
+    "groupName": "Balikó család",
+    "groupList":[
+      {"nick" : "Zsófi", "name" : "Balikó Zsófia"},
+      {"nick" : "Rózi", "name" : "Balikó Róza"},
+      {"nick" : "Kis Tomi", "name" : "Balikó Tamás"},
+      {"nick" : "Aliz", "name" : "Kiss Aliz"},
+      {"nick" : "Zozó", "name" : "Balikó Zoltán"},
+      {"nick" : "Elvike", "name" : "Balikóné Balás Elvira"},
+      {"nick" : "Zoli", "name" : "Balikó Zoltán"}
+    ]
+  },
+  {
+    "groupName": "Balás család",
+    "groupList":[
+      {"nick" : "Bálintka", "name" : "Balás Bálint"},
+      {"nick" : "Etelka", "name" : "Dr. Balásné Rozs Etelka"},
+      {"nick" : "Bandi", "name" : "Balás Endre"},
+      {"nick" : "Bandika", "name" : "Balás Endre"},
+      {"nick" : "Bence", "name" : "Balás Benedek"},
+      {"nick" : "Oti", "name" : "Balás Otília"},
+      {"nick" : "Eti", "name" : "Balás Etelka"}
+    ]
+  },
+  {
+    "groupName": "Kovács család",
+    "groupList":[
+      {"nick" : "Verocska", "name" : "Kovács Vera Anikó"},
+      {"nick" : "Karina", "name" : "Kovács Karina"},
+      {"nick" : "Máté", "name" : "Nagypál Máté"},
+      {"nick" : "Kíra", "name" : "Kovács Kíra Diána"}
+    ]
+  },
+  {
+    "groupName": "Tóbel család",
+    "groupList":[
+      {"nick" : "Donát", "name" : "Gerencsér Donát"},
+      {"nick" : "Szotyi", "name" : "Tóbel Szonja"},
+      {"nick" : "Szabcsi", "name" : "Tóbel Szabina"},
+      {"nick" : "Eda", "name" : "Tóbelné Hétvári Edina"},
+      {"nick" : "Szabi", "name" : "Tóbel Szabolcs"},
+      {"nick" : "Szofi", "name" : "Tóbel Szófia"}
+    ]
+  },
+  {
+    "groupName": "Nagy család, Kiss-Tóth család és Győri család",
+    "groupList":[
+      {"nick" : "Ferki", "name" : "Nagy Ferenc"},
+      {"nick" : "Kica", "name" : "Sólyom Katalin"},
+      {"nick" : "Ágó", "name" : "Nagy Ágoston"},
+      {"nick" : "Olcsi", "name" : "Nagy Olívia"},
+      {"nick" : "Babika", "name" : "Nagy Anna"},
+      {"nick" : "Blani", "name" : "Kiss-Tóth Blanka"},
+      {"nick" : "Hani", "name" : "Kiss-Tóth Hanna"},
+      {"nick" : "Dani", "name" : "Bús Dániel"},
+      {"nick" : "Kati néni!", "name" : "Dr. Pálfalvi Sándorné Katalin"},
+      {"nick" : "Márti", "name" : "Győriné Pálfalvi Márta"},
+      {"nick" : "Csaba", "name" : "Győri Csaba"},
+      {"nick" : "Máté", "name" : "Győri Máté"},
+      {"nick" : "Kristóf", "name" : "Győri Kristóf"}
+    ]
+  },
+  {
+    "groupName": "Varga Tamás és család",
+    "groupList":[
+      {"nick" : "Borci", "name" : "Varga Boróka"},
+      {"nick" : "Zsolt", "name" : "Forrai Zsolt"},
+      {"nick" : "Levi", "name" : "Varga Levente"},
+      {"nick" : "Kriszti", "name" : "Dr. Varga Tamásné"},
+      {"nick" : "Jembi", "name" : "Dr. Varga Tamás"}
+    ]
+  },
+  {
+    "groupName": "Edzők és párjaik",
+    "groupList":[
+      {"nick" : "Zsuzsa néni", "name" : "Fazekas Gáspárné"},
+      {"nick" : "Gazsi bácsi", "name" : "Fazekas Gáspár"},
+      {"nick" : "Sanyi!", "name" : "Vasvári Sándor"},
+      {"nick" : "Kati néni", "name" : "Tarrné Naszádos Katalin"},
+      {"nick" : "Imre bácsi", "name" : "Tarr Imre"},
+      {"nick" : "Nóri", "name" : "Csüllög Nóra"},
+      {"nick" : "Andris!", "name" : "Szabó András"},
+      {"nick" : "Berkó", "name" : "Berkovics Imre"},
+      {"nick" : "Zsuzsi", "name" : "Vajda Zsuzsi"},
+      {"nick" : "Jani bácsi", "name" : "Zemen János"}
+    ]
+  },
+  {
+    "groupName": "Miklós Tamás és családja",
+    "groupList":[
+      {"nick" : "Barabás", "name" : "Miklós Barabás"},
+      {"nick" : "Benedek", "name" : "Miklós Benedek"},
+      {"nick" : "Edit", "name" : "Miklós Edit"},
+      {"nick" : "Tomi", "name" : "Miklós Tamás"}
+    ]
+  },
+  {
+    "groupName": "Valaha volt DOVASÉSOK",
+    "groupList":[
+      {"nick" : "Sziszi", "name" : "Tamás Szilárd"},
+      {"nick" : "Andi", "name" : "Tamás Szilárdné"},
+      {"nick" : "Zsófi", "name" : "Tamás Zsófia"},
+      {"nick" : "Dóri!", "name" : "Börcsök Dóra"},
+      {"nick" : "Dávid", "name" : "Bakos Dávid"},
+      {"nick" : "Dóri", "name" : "Tonács Dóra"},
+      {"nick" : "Gréti", "name" : "Harmati Gréta"},
+      {"nick" : "Zalán", "name" : "Uzonyi Zalán"},
+      {"nick" : "Tomi", "name" : "Pelcz Tamás"},
+      {"nick" : "Lilike", "name" : "Pelcz Lili"},
+      {"nick" : "Zsani", "name" : "Beke Zsanett"},
+      {"nick" : "Luca", "name" : "Jászberényi Luca"},
+      {"nick" : "Anthony", "name" : "Anthony Dill"}
+    ]
+  },
+  {
+    "groupName": "Futók és párjaik",
+    "groupList":[
+      {"nick" : "Szabina", "name" : "Imre Szabina"},
+      {"nick" : "Ricsi", "name" : "Potondi Richárd"},
+      {"nick" : "Anna", "name" : "Pataki Anna"},
+      {"nick" : "Dávid", "name" : "Pataki Dávid"},
+      {"nick" : "Marci", "name" : "Pápai Márton"},
+      {"nick" : "Kinga", "name" : "Ohn Kinga"},
+      {"nick" : "Dárius", "name" : "Farkas Dárius"},
+      {"nick" : "Milán", "name" : "Fazekas Milán"},
+      {"nick" : "Palkó", "name" : "Palkovits István"},
+      {"nick" : "Bálint", "name" : "Kozma Bálint"},
+      {"nick" : "Lóci", "name" : "Varga Lőrinc"},
+      {"nick" : "Bálintka", "name" : "Szinte Bálint"},
+      {"nick" : "Márkiboy", "name" : "Vörös Márk"},
+      {"nick" : "Bácska", "name" : "Bácskai Bence"},
+      {"nick" : "Benő", "name" : "Kelemen Bence"},
+      {"nick" : "Réka", "name" : "Faragó Réka"},
+      {"nick" : "Nóri", "name" : "Csiszár Nóra"},
+      {"nick" : "Csabi", "name" : "Matkó Csaba"},
+      {"nick" : "Ricsi", "name" : "Süli Richárd"},
+      {"nick" : "Fanni", "name" : "Barna Fanni"},
+    ]
+  },
+  {
+    "groupName": "Hintások és Atitiszi",
+    "groupList":[
+      {"nick" : "Atitiszi", "name" : "Kutas Attila"},
+      {"nick" : "Géza bácsi", "name" : "Magyarfalvi Géza"},
+      {"nick" : "Kriszti néni", "name" : "Magyarfalvi Krisztina"},
+      {"nick" : "Viki", "name" : "Jakab Viktória"},
+      {"nick" : "Henrik", "name" : "Seres Henrik"},
+      {"nick" : "Szabi", "name" : "Kiss Szabolcs"},
+      {"nick" : "Gréti", "name" : "Farkas Gréti"},
+      {"nick" : "Csani", "name" : "Kiss Csanád"},
+      {"nick" : "Eszti", "name" : "Kissné Magyarfalvi Eszter"},
+      {"nick" : "Annamari", "name" : "Vörös Annamária"},
+      {"nick" : "Kristóf", "name" : "Kéri Kristóf"},
+      {"nick" : "Kozi", "name" : "Kozák Bence"},
+      {"nick" : "Valentin", "name" : "Ács Valentin"},
+      {"nick" : "Dalma", "name" : "Gelencsér Dalma"},
+      {"nick" : "Barnus", "name" : "Vissi Barnabás"},
+      {"nick" : "Gábor", "name" : "Hohmann Gábor"},
+      {"nick" : "Zsófi", "name" : "Hohmann-Tóth Zsófia"},
+    ]
+  }
+]
+
+let mainContainer = document.getElementById("mainContainer")
+
+for (var i = 0; i < groups.length; i++) {
+  currentGroup = document.createElement("div");
+  currentGroup.classList.add('card');
+  mainContainer.appendChild(currentGroup);
+
+  currentGroupHeader = document.createElement("div");
+  currentGroupHeader.classList.add('card-header');
+  currentGroupHeader.innerHTML = groups[i]["groupName"]
+  currentGroup.appendChild(currentGroupHeader);
+
+  currentGroupBody = document.createElement("div");
+  currentGroupBody.classList.add('card-body');
+  currentGroup.appendChild(currentGroupBody);
+
+  currentGroupBodyRow = document.createElement("div");
+  currentGroupBodyRow.classList.add('row');
+  currentGroupBody.appendChild(currentGroupBodyRow);
+
+  for (var j = 0; j < groups[i]["groupList"].length; j++) {
+    currentSize = document.createElement("div");
+    currentSize.classList.add('col-6');
+    currentSize.classList.add('col-sm-4');
+    currentSize.classList.add('col-md-3');
+    currentSize.classList.add('col-xl-2');
+    currentSize.classList.add('name_photo');
+    currentGroupBodyRow.appendChild(currentSize);
+
+    currentNameCard = document.createElement("div");
+    currentNameCard.classList.add('card');
+    currentSize.appendChild(currentNameCard);
+
+    currentNameCardBody = document.createElement("div");
+    currentNameCardBody.classList.add('card-body');
+    currentNameCard.appendChild(currentNameCardBody);
+
+    currentNick = document.createElement("h5");
+    currentNick.classList.add('card-title');
+    currentNick.innerHTML = groups[i]["groupList"][j]["nick"]
+    currentNameCardBody.appendChild(currentNick);
+
+    currentName = document.createElement("p");
+    currentName.classList.add('card-text');
+    currentName.innerHTML = groups[i]["groupList"][j]["name"]
+    currentNameCardBody.appendChild(currentName);
+  }
+}
